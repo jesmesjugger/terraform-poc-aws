eb4a17b53ae427130ec09c52d97a32c0e0acdb88
Martin Mkolwe <martinjj80@gmail.com>
2023-01-18 13:37:21 +0300

    Update providers.tf
    Commenting out credentials

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.aws_region
-  secret_key = var.aws_access-now
-  access_key = var.aws_access-id
+  #secret_key = var.aws_access-now
+  #access_key = var.aws_access-id
 }
